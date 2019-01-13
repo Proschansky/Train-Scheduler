fe1eb81e72ec3f8a768c775a5fd49894d2cadb7b
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-01-13 16:49:54 -0500

    I updated the index and the script so that it works. I added a config.js to the gitignore to hide the API key. I still need to fix the minutes away as it is not working yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,69 @@ var login = {
     authDomain: config.authDomain,
     databaseURL: config.databaseURL,
     projectId: config.projectId,
-    messagingSenderId: config.messagingSenderId
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
   };
-  
+
   firebase.initializeApp(login);
+
+  var database = firebase.database();
+
+$("#trainInfoBtn").on("click", function(event) {
+    event.preventDefault(); 
+
+    var trainName = $("#name").val().trim();
+    var destination = $("#dest").val().trim();
+    var frequency = $("#freq").val().trim();
+    var initialTime = moment($("#initialTime").val().trim(), "hh:mm").subtract(1, "years").format("X");
+        
+    var currentTime = moment();
+    console.log("CURRENT TIME: " +  moment(currentTime).format("hh:mm"));
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(initialTime);
+    console.log(frequency);
+    console.log(currentTime);
+
+    var newTrain = {
+        train: trainName,
+        trainDest: destination,
+        trainArrival: initialTime,
+        everyMin: frequency
+    };
+
+    database.ref().push(newTrain);
+
+}); 
+
+database.ref().on("child_added", function(childSnapshot) {
+
+        console.log(childSnapshot.val());
+      
+        var trainName = childSnapshot.val().train;
+        var destination =childSnapshot.val().trainDest;
+        var initialTime = childSnapshot.val().trainArrival;
+        var frequency = childSnapshot.val().everyMin;
+
+        var trainTime = moment(initialTime).format("hh:mm");
+       
+        var difference =  moment().diff(moment(trainTime),"minutes");
+
+        var trainRemain = difference % frequency;
+        console.log(trainRemain);
+
+  
+        var minRemain = frequency - trainRemain;
+        console.log(minRemain);
+
+   
+        var nextArrival = moment().add(minRemain, "minutes").format('hh:mm');
+        console.log(nextArrival);
+
+        $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+             frequency + "</td><td>" + nextArrival + "</td><td>" + minRemain + "</td></tr>");
+
+});
+
+  
